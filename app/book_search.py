036d3c0e210981c8881064e04f902e3f1c55c6a4
danielazecheru <dcz218@stern.nyu.edu>
2025-08-06 21:45:07 -0400

    Enhanced web app functionalities

---
+++ app/book_search.py
@@ -0,0 +1,60 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+nyt_api_key = os.getenv("NYT_API_KEY")
+
+load_dotenv()
+openai_api_key= os.getenv("OPENAI_API_KEY")
+
+import requests
+
+from openai import OpenAI # type: ignore
+
+url = f"https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key={nyt_api_key}"
+r = requests.get(url)
+books = r.json()
+
+lists =[]
+
+for list in books["results"]["lists"]:
+    x = {"list_name": list["list_name"], "list_name_encoded":list["list_name_encoded"]}
+    lists.append(x)
+
+
+def fetch_bestselling_list(genre):
+    for list in lists:
+        if genre == list["list_name"]:
+            genre_encoded =list["list_name_encoded"]
+    url = f"https://api.nytimes.com/svc/books/v3/lists/{genre_encoded}.json?api-key={nyt_api_key}"
+    r = requests.get(url)
+    books = r.json()
+    return books
+
+
+if __name__ == "__main__":
+
+    genre = input("Please enter a genre you are interested in: ")
+
+    print(r.status_code)
+    books = r.json()
+    print("NYT Bestselling list for", genre)
+    print( )
+    for book in books["results"]["books"]:
+        print(book["rank"], "-", book["title"], "by", book["author"])
+        print("Description:", book["description"])
+        print("Weeks spent on list:", book["weeks_on_list"])
+        print("Buy on Amazon:", book["buy_links"][0]["url"])
+        print( )
+
+
+    book = input("Please enter a book you want to learn more about: ")
+
+    client = OpenAI(api_key=openai_api_key)
+
+    response = client.responses.create(
+        model="gpt-4.1",
+        input=f"Without giving any plot spoilers, summarize in no more than 3 sentences the book reviews for {book}"
+    )
+
+    print(response.output_text)
--- web-app/__init__.py
+++ web-app/__init__.py
@@ -1,11 +1,25 @@
 from flask import Flask, render_template, request
 
+from app.book_search import fetch_bestselling_list 
+
 app = Flask(__name__)
 
 # route for home page
 @app.route('/')
 def home():
     return render_template("home.html")
+
+@app.route('/results', methods=['POST'])
+def results():
+
+    user_choice = request.form.get('genre')
+
+    print(request.form)
+
+    return render_template("results.html",
+        user_choice=user_choice,
+        bestselling_list = fetch_bestselling_list(user_choice)
+    )
 
 
 if __name__ == '__main__':
